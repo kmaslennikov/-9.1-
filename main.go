e847c4a833455e25e0f8d9a1df91977f80baed7f
kmaslennikov <82436599+kmaslennikov@users.noreply.github.com>
2022-01-14 12:45:35 +0000

    overflow

--- main.go
+++ main.go
@@ -9,10 +9,9 @@ func main() {
 	var counterUint8, counterUint16 int
 	var tempUint8 uint8
 	var tempuint16 uint16
-	for i := 0; i <= math.MaxUint32; i++ {
+	for i := 0; i < math.MaxUint32; i++ {
 		if tempUint8 == math.MaxUint8 {
 			counterUint8++
-
 		}
 		if tempuint16 == math.MaxUint16 {
 			counterUint16++
