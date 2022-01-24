866f2f8770ab55ecb68bf3108f249553201a230b
kmaslennikov <82436599+kmaslennikov@users.noreply.github.com>
2022-01-24 13:31:36 +0000

    timeout

--- main.go
+++ main.go
@@ -7,19 +7,14 @@ import (
 
 func main() {
 	var counterUint8, counterUint16 int
-	var tempUint8 uint8
-	var tempuint16 uint16
-	for i := 0; i < math.MaxUint32; i++ {
-		if tempUint8 == math.MaxUint8 {
-			counterUint8++
-		}
-		if tempuint16 == math.MaxUint16 {
-			counterUint16++
-		}
-		tempUint8++
-		tempuint16++
+
+	for i := math.MaxUint32; i > 0; i -= math.MaxUint8 {
+		counterUint8++
+	}
+	for i := math.MaxUint32; i > 0; i -= math.MaxUint16 {
+		counterUint16++
 	}
 	fmt.Printf("%d приходится переполнений чисел типа uint8 в диапазоне от 0 до uint32\n", counterUint8)
+	fmt.Printf("%d приходится переполнений чисел типа uint8 в диапазоне от 0 до uint32\n", counterUint16)
 
-	fmt.Printf("%d приходится переполнений чисел типа uint16 в диапазоне от 0 до uint32\n", counterUint16)
 }
